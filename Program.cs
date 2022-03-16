0165a8b236c45f147aa3baa24d0392b3a8035137
TSYMarina <marinatsy16@gmail.com>
2022-03-16 10:12:41 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace KanyeAndRon
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's get some wisdom from Ron Swanson and Kanye West. \n\n\n");
+
+            QuoteGenerator.KanyeQuote();
+            QuoteGenerator.RonQuote();
+            QuoteGenerator.KanyeQuote();
+            QuoteGenerator.RonQuote();
+
+            Console.WriteLine("\n\n**Still enjoying it? Press 1 to continue or 0 to stop the fun.");
+
+            bool cont = true;
+            var userResponse = Console.ReadLine();
+
+            if (userResponse == "1")
+            {
+                cont = true;
+            }
+            else
+            {
+                cont = false;
+                Environment.Exit(0);
+            }
+
+            do
+            {
+                QuoteGenerator.KanyeQuote();
+                QuoteGenerator.RonQuote();
+                QuoteGenerator.KanyeQuote();
+                QuoteGenerator.RonQuote();
+                QuoteGenerator.KanyeQuote();
+                QuoteGenerator.RonQuote();
+
+                Console.WriteLine("\n\n**Still enjoying it? Press 1 to continue or 0 to stop the fun.");
+                userResponse = Console.ReadLine();
+                Console.ReadKey();
+
+            }
+            while (cont == true);
+        }
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyesURL = "https://api.kanye.rest";
+            var kanyesResponse = client.GetStringAsync(kanyesURL).Result;
+            var kanyesQuote = JObject.Parse(kanyesResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"\nKanye says:\n   { kanyesQuote}.");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronsURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronsResponse = client.GetStringAsync(ronsURL).Result;
+            var ronsQuote = JArray.Parse(ronsResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+            Console.WriteLine($"\nRon responds:\n   {ronsQuote}");
+        }
+
+    }
+}
